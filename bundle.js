043efd2e1c19629b45cad8a8df7043db2bcf4247
sun-dive <chainsaw@bzzapp.com>
2026-06-21 09:30:16 +0800

    Cover image: contain (no crop) for all aspect ratios

--- bundle.js
+++ bundle.js
@@ -27083,7 +27083,7 @@ This INVALIDATES those links and returns their pre-funded sats to your wallet (m
   function init() {
     store2 = new PharLapStore();
     const ver = $("appVersion");
-    if (ver != null) ver.textContent = `Smart NFTs \xB7 v${"0.1"} \xB7 ${"dc68bfd"} \xB7 ${"2026-06-20"}`;
+    if (ver != null) ver.textContent = `Smart NFTs \xB7 v${"0.1"} \xB7 ${"6ca396b"} \xB7 ${"2026-06-21"}`;
     loadAliases();
     const watch = localStorage.getItem(WATCH_KEY);
     if (watch != null) {
